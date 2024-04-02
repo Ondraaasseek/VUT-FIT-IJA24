fb5e7031e3912d4d1d1163abff88dedb131b0af6
Lukas Katona <katona.lukasek@gmail.com>
2024-04-02 13:02:28 +0200

    feat(tool.common): added Observable.java and inner Observer interface

---
+++ homework/ija/ija2023/homework2/tool/common/Observable.java
@@ -0,0 +1,12 @@
+package ija.ija2023.homework2.tool.common;
+
+public interface Observable {
+    void addObserver(Observable.Observer o);
+    void removeObserver(Observable.Observer o);
+    void notifyObservers();
+
+    // Observer interface
+    public static interface Observer {
+        void update(Observable o);
+    }
+}
