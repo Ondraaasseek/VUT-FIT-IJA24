cedf4e991534c78deb28dce83982ab68d500e820
Ondřej Novotný <ondronicek@gmail.com>
2024-04-02 13:34:18 +0200

    Feat: Env and View prototypes

---
+++ homework/ija/ija2023/homework2/tool/EnvPresenter.java
@@ -0,0 +1,19 @@
+package ija.ija2023.homework2.tool;
+
+import ija.ija2023.homework2.tool.view.FieldView;
+import ija.ija2023.homework2.common.Position;
+public class EnvPresenter {
+
+    EnvPresenter(ToolEnvironment env){
+        // Constructor initializes the Presenter
+        // ENV -> Environment that is being presented
+    }
+
+    FieldView fieldAt(Position pos){
+
+    }
+
+    void open(){
+        // Creates and opens GUI
+    }
+}
---
+++ homework/ija/ija2023/homework2/tool/view/ComponentView.java
@@ -0,0 +1,5 @@
+package ija.ija2023.homework2.tool.view;
+
+public interface ComponentView {
+
+}
