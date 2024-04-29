e4e09efb513a133ac9f8234055fe0663b5a22fe9
Ondřej Novotný <ondronicek@gmail.com>
2024-04-29 13:50:13 +0200

    feat: design edit

--- src/main/java/ija/ija2023/project/InputInterface.java
+++ src/main/java/ija/ija2023/project/InputInterface.java
@@ -1,9 +1,19 @@
 package ija.ija2023.project;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.GridPane;
+import javafx.geometry.Insets;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 
@@ -13,8 +23,12 @@ public class InputInterface extends Application {
         primaryStage.setTitle("Robot Simulation");
         Button button1 = new Button("Load from file");
         Button button2 = new Button("Create new environment");
+        Text text = new Text("Roboti a Kameny");
+        text.setFont(Font.font("null", FontWeight.BOLD, 20));
 
-        VBox vbox = new VBox(button1, button2);
+        VBox vbox = new VBox(text, button1, button2);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(10);
         Scene scene = new Scene(vbox, 300, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -30,12 +44,37 @@ public class InputInterface extends Application {
             // - Height
             Stage NewEnvStage = new Stage();
             NewEnvStage.setTitle("Create new environment");
+
+            Text textEnv = new Text("Create new environment");
+            textEnv.setFont(Font.font("null", FontWeight.BOLD, 20));
+
+            GridPane gridPane = new GridPane();
+            gridPane.setHgap(10); // Horizontal gap
+            gridPane.setVgap(10); // Vertical gap
+            gridPane.setPadding(new Insets(10, 10, 10, 10)); // Padding
+
+            Label widthLabel = new Label("Width:");
             TextField widthField = new TextField();
+            gridPane.add(widthLabel, 0, 0); // Add to column 0, row 0
+            gridPane.add(widthField, 1, 0); // Add to column 1, row 0
+
+            Label heightLabel = new Label("Height:");
             TextField heightField = new TextField();
+            gridPane.add(heightLabel, 0, 1); // Add to column 0, row 1
+            gridPane.add(heightField, 1, 1); // Add to column 1, row 1
+
             Button createButton = new Button("Create");
             Button cancelButton = new Button("Cancel");
 
-            VBox newEnvVbox = new VBox(widthField, heightField, createButton, cancelButton);
+            Region spacer = new Region();
+            HBox.setHgrow(spacer, Priority.ALWAYS);
+
+            HBox buttonBox = new HBox(cancelButton, spacer, createButton);
+            buttonBox.setPadding(new Insets(10, 10, 10, 10)); // Padding
+
+            VBox newEnvVbox = new VBox(textEnv, gridPane, buttonBox);
+            newEnvVbox.setAlignment(Pos.CENTER);
+            newEnvVbox.setSpacing(10);
             Scene newEnvScene = new Scene(newEnvVbox, 300, 200);
             NewEnvStage.setScene(newEnvScene);
             // Close the old windows
