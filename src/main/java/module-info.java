ce2420a8ddd5791f22df23cb92b239336a01c8d5
Ondřej Novotný <ondronicek@gmail.com>
2024-05-03 16:40:49 +0200

    feat: program can now be build and runned.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,11 @@
 module IJA{
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
     requires java.desktop;
     requires javafx.base;
     requires java.xml;
-    requires jdk.internal.le;
+    requires javafx.graphics;
 
     opens ija.ija2023.project to javafx.fxml;
     exports ija.ija2023.project.env to javafx.graphics;
-    exports ija.ija2023.project.room to javafx.graphics;
-    exports ija.ija2023.project.common to javafx.graphics;
-    exports ija.ija2023.project.common.robot to javafx.graphics;
 }
